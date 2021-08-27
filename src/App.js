1cd784f1eb953aeae73b6235aee663c304fdad42
Bengt Uggla <bengt.uggla@gmail.com>
2021-08-27 13:58:27 +0200

    Adding Header Component with styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import Homepage from './components/pages/homepage/Homepage';
 import ShopPage from './components/pages/shop/ShopPage';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShopPage}/>
       </Switch>
+      
      
     </div>
   );
