a9c8f35fd4a310559e90ce428be1855ba96fb84a
Bengt Uggla <bengt.uggla@gmail.com>
2021-08-27 10:43:57 +0200

    Updating with shop page, preview component, collection item comp. + styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
+import { Route, Switch } from 'react-router';
 import './App.css';
-import Homepage from './components/Homepage';
+import Homepage from './components/pages/homepage/Homepage';
+import ShopPage from './components/pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
+     
     </div>
   );
 }
---
+++ src/components/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../directory/Directory'
+
+const Homepage = () => {
+ return (
+  <div className="homePage"> 
+  <Directory />
+  </div>
+ )
+}
+
+export default Homepage
